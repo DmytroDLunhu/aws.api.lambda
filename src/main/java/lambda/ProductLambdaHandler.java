3a29a01d0ea25172036fe93b79edcfe401f210fc
Dmytro Lunhu <dlunhu@gmail.com>
2023-02-21 16:43:49 -0500

    add .jar file, crud methods

--- src/main/java/lambda/ProductLambdaHandler.java
+++ src/main/java/lambda/ProductLambdaHandler.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import model.Product;
@@ -59,6 +60,70 @@ public class ProductLambdaHandler implements RequestStreamHandler {
             }
         } catch (ParseException e) {
             throw new RuntimeException(e);
+        }
+        writer.write(responseObject.toString());
+        reader.close();
+        writer.close();
+    }
+
+    public void handlePutRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        OutputStreamWriter writer = new OutputStreamWriter(outputStream);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        JSONParser parser = new JSONParser();           // parse request object
+        JSONObject responseObject = new JSONObject();   // add object to API response
+        JSONObject responseBody = new JSONObject();     // add item to objectL
+
+        AmazonDynamoDB client = AmazonDynamoDBAsyncClientBuilder.defaultClient();
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        try {
+            JSONObject requestObject = (JSONObject) parser.parse(reader);
+            if (requestObject.get("body") != null) {
+                Product product = new Product((String) requestObject.get("body"));
+
+                dynamoDB.getTable(DYNAMO_TABLE)
+                        .putItem(new PutItemSpec().withItem(new Item()
+                                .withNumber("id", product.getPrice())
+                                .withString("name", product.getName())
+                                .withNumber("price", product.getPrice())));
+                responseBody.put("message", "New Item created/updated");
+                requestObject.put("statusCode", 200);
+                responseBody.put("body", responseBody.toString());
+            }
+        } catch (Exception e) {
+            responseObject.put("statusCode", 400);
+            responseObject.put("ERROR", e);
+        }
+        writer.write(responseObject.toString());
+        reader.close();
+        writer.close();
+    }
+
+    public void handleDeleteRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        OutputStreamWriter writer = new OutputStreamWriter(outputStream);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        JSONParser parser = new JSONParser();           // parse request object
+        JSONObject responseObject = new JSONObject();   // add object to API response
+        JSONObject responseBody = new JSONObject();     // add item to objectL
+
+        AmazonDynamoDB client = AmazonDynamoDBAsyncClientBuilder.defaultClient();
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        try {
+            JSONObject requestObject = (JSONObject) parser.parse(reader);
+            if (requestObject.get("pathParams") != null) {
+                JSONObject pps = (JSONObject) requestObject.get("pathParams");
+                if (pps.get("id") != null) {
+                    int id = Integer.parseInt((String) pps.get("id"));
+                    dynamoDB.getTable(DYNAMO_TABLE).deleteItem("id", id);
+                }
+            }
+            responseBody.put("message", "Item deleted");
+            responseObject.put("statusCode", 200);
+            responseObject.put("body", responseBody.toString());
+        } catch (Exception e) {
+            responseObject.put("statusCode", 400);
+            responseObject.put("ERROR", e);
         }
         writer.write(responseObject.toString());
         reader.close();
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello AWS world!");
+        System.out.println("Hello AWS-CRUD-API world!");
     }
 }
